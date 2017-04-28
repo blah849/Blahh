4e5fba8921ba4efb13ad92f0b54389a4228fa6f9
blah849 <winterfunsum@gmail.com>
2017-04-28 17:05:26 -0400

    Update Note.java

--- src/adventure/things/generic/Note.java
+++ src/adventure/things/generic/Note.java
@@ -6,9 +6,9 @@ public class Note extends Thing {
     public Note() {
         shortName = "Note";
         fullName = "Note";
-        description = "Sorry I put you in this situation but i owe mike over $1,000\n" +
+        description = "Sorry I put you in this situation, but I owe Mike over $1,000\n" +
                 "and he isn't too happy about it. If anything happens to me just know\n" +
                 "he’s the guy with the scar under his left eye. I also put his apartment key" +
-                "\nunder the couch cushion so you can have access to his apartment if you ever need to.";
+                "\nunder the couch cushion so you can access his apartment.";
     }
 }
