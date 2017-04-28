d1e9ee67d3da19050d43efe0688b2c2ceeef4d0f
blah849 <winterfunsum@gmail.com>
2017-04-28 17:07:27 -0400

    Update Magazine.java

--- src/adventure/things/interactive/Magazine.java
+++ src/adventure/things/interactive/Magazine.java
@@ -7,10 +7,10 @@ public class Magazine extends Thing {
         this.shortName = "Magazine";
         this.fullName = "Magazine";
         this.description = "Welcome to Gotham's, Dark Agent. " +
-                "This game is for a true detective, you must have" +
+                "This game is for the true detective, you must have" +
                 " what it takes to take down real criminals.\n" +
-                "You will be dropped into the middle of a crime scene" +
+                "You'll be dropped into the middle of a crime scene" +
                 " and everyone will be counting on you to get " +
-                "to the bottom of it and arrest the correct criminal.";
+                "to the bottom of it and arrest the right guy.";
     }
 }
